86d13cc59cafbb187f06abd683835df9401bc5d6
waitingzhao <wtz206@nyu.edu>
2023-11-29 16:50:56 -0500

    clean data

--- data.py
+++ data.py
@@ -16,7 +16,6 @@ data['Performance'] = data['Performance'].str.replace('PISA: Mean performance on
 data['Gender'] = data['Gender'].fillna('Average').str.strip()
 df = data.drop('Series Name', axis=1)
 
-
 #Remove columns of '2013 [YR2013]' and '2014 [YR2014]'
 data.drop(['2013 [YR2013]', '2014 [YR2014]'], axis=1, inplace=True)
 
@@ -28,10 +27,19 @@ data.reset_index(drop=True, inplace=True)
 data.drop(index=[1161, 1162], inplace=True)
 #st.write(data)
 
-#Clean up all the rows where '2015 [YR2015]' is None.
+#Clean up all the rows where '2015 [YR2015]' is None and rename it to '2015'
 data = data.dropna(subset=['2015 [YR2015]'])
+data.rename(columns={'2015 [YR2015]': 'Year of 2015'}, inplace=True)
+
 #st.write(data)
 
 #Hide the column of 'Series Name' to see the final data I need.
 data_view = data.drop(columns=['Series Name'])
+#Show the cleaned data I will use for my project.
 st.write(data_view)
+
+#Group the data by country and performance, then calculate the mean of the scores.
+mean_scores = data_view.groupby(['Country Name', 'Performance'])['Year of 2015'].mean().reset_index()
+#Create a bar chart
+fig = px.bar(mean_scores, x='Country Name', y='Year of 2015', color='Performance', title='PISA Mean Scores by Country and Subject (Year of 2015)')
+st.plotly_chart(fig)
